3db499f0c801945adf58808afba733074c0016f3
myself-rasel-mahmud <md.rasel.mahmud.mail@gmail.com>
2023-05-01 22:44:14 +0600

    Initialize client site project

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <h2>Assignment-10 😢😢</h2>
+  </React.StrictMode>,
+)
